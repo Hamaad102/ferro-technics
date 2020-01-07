e8fdc9e3e102d2f4ee1ab67f0db73e2f6de406b8
Hamaad Chughtai <Hamaad102@gmail.com>
2020-01-07 05:10:55 -0500

    Added stylesheet, began work on navbar

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 
 import Home from './home';
 import Courses from './courses';
@@ -7,31 +7,46 @@ import Enterprise from './enterprise';
 import Contact from './contact';
 import Register from './register';
 
+import '../style/app.css';
+
 function App() {
+
   return (
       <Router>
+        <div className="container">
+          
+          <navbar>
 
-        <div className="nav">
+            <Link className='brand' to='/'>Ferro Technics</Link>
+            
+            <ul className='navLinks'>
+              <li><Link to='/courses'>Courses</Link></li>
+              <li><Link to='/enterprise'>Enterprise</Link></li>
+              <li><Link to='/contact'>Contact</Link></li>
+              <li><Link to='/register'>Register</Link></li>
+            </ul>
+
+          </navbar>
+          
+          <Switch>
+            <Route path='/'>
+              <Home />
+            </Route>
+            <Route path='/courses'>
+              <Courses />
+            </Route>
+            <Route path='/enterprise'>
+              <Enterprise />
+            </Route>
+            <Route path='/contact'>
+              <Contact />
+            </Route>
+            <Route path='/register'>
+              <Register />
+            </Route>
+          </Switch>
+
         </div>
-
-        <Switch>
-          <Route path='/'>
-            <Home />
-          </Route>
-          <Route path='/courses'>
-            <Courses />
-          </Route>
-          <Route path='/enterprise'>
-            <Enterprise />
-          </Route>
-          <Route path='/contact'>
-            <Contact />
-          </Route>
-          <Route path='/register'>
-            <Register />
-          </Route>
-        </Switch>
-
       </Router>
   );
 }
