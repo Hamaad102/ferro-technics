a661b4d73c6539aea9f4f07ab49bbc7436dee3bf
Hamaad Chughtai <Hamaad102@gmail.com>
2020-01-08 04:02:55 -0500

    Updates

---
+++ precache-manifest.6a47ea5e3928663ee96d7f52997a4740.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "802fe806f8762a003cad0705fb5581b9",
+    "url": "/ferro-technics/index.html"
+  },
+  {
+    "revision": "60f4ac2d5fa0d8285bf5",
+    "url": "/ferro-technics/static/css/main.69e1775d.chunk.css"
+  },
+  {
+    "revision": "627dcfd0c9ae07447d5a",
+    "url": "/ferro-technics/static/js/2.e4676957.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/ferro-technics/static/js/2.e4676957.chunk.js.LICENSE"
+  },
+  {
+    "revision": "60f4ac2d5fa0d8285bf5",
+    "url": "/ferro-technics/static/js/main.0f09ad0e.chunk.js"
+  },
+  {
+    "revision": "7a657e5844f8d9ca33ab",
+    "url": "/ferro-technics/static/js/runtime-main.c9442a6b.js"
+  }
+]);
